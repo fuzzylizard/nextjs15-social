0867359c9c2ded938c97227cd72b14f56723781b
Christopher Johnston <chris@fuzzylizard.com>
2025-01-17 15:14:35 -0500

    Back for authentication

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   experimental: {
     staleTimes: {
       dynamic: 30,
     },
   },
+  serverExternalPackages: ["@node-rs/argon2"],
   /* config options here */
 };
 
